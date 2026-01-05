ed3c7c9f7239bd19bbf420774ac5c9fc439c72bb
Rakibul Islam <rakibulislamarif793@gmail.com>
2026-01-05 22:28:37 +0600

    first commit

---
+++ day_01/basic.py
@@ -0,0 +1,14 @@
+
+import numpy as np
+
+
+
+# c = np.array([
+#     [10,20,30],
+#     [40,50,60]
+# ])
+
+# print(c[:,-1])  # Output: [20 50]
+
+a = np.array([10, 20, 30, 40, 50])
+print(a>25)
---
+++ day_01/condition_indexing.py
@@ -0,0 +1,4 @@
+import numpy as np
+x = np.array([5, 12, 25, 7, 30])
+
+print(x<10)
