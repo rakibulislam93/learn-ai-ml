17ef5c8dc396cd0efe477f0566b57dfd3ce75c16
Rakibul Islam <rakibulislamarif793@gmail.com>
2026-01-06 23:55:08 +0600

    add new file

--- day_01/condition_indexing.py
+++ day_01/condition_indexing.py
@@ -1,4 +1,16 @@
 import numpy as np
 x = np.array([5, 12, 25, 7, 30])
 
-print(x<10)
+print(x[x > 15])  # Output: [20 30]
+
+
+
+c = np.array([
+    [10, 20, 3],
+    [40, 50, 60]
+])
+
+print(c[c > 10])
+
+result = np.where(c%2 == 0, 'Even', 'Odd')
+print(result)
---
+++ day_01/main.py
@@ -0,0 +1,2 @@
+
+print("Start my numPy journey!")
